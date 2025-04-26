ce5a7cecf80d9690dfc84c3caad7d27a5ec19b27
NgThanhTrung124 <ngtrungbo06@gmail.com>
2025-04-26 20:33:38 +0700

    Add Particles JS and responsible for Full screen

---
+++ app.js
@@ -0,0 +1,26 @@
+particlesJS('particles-js', {
+                particles: {
+                    number: {
+                        value: 70,
+                        density: { enable: true, value_area: 800 }
+                    },
+                    color: { value: '#FFAE00' },
+                    shape: { type: 'circle' },
+                    opacity: { value: 0.8, random: true },
+                    size: { value: 3, random: true },
+                    line_linked: {
+                        enable: true,
+                        distance: 150,
+                        color: '#FFAE00',
+                        opacity: 0.2,
+                        width: 1
+                    },
+                    move: {
+                        enable: true,
+                        speed: 3,
+                        direction: 'none',
+                        random: true,
+                        out_mode: 'out'
+                    }
+                }
+            });
